fc9b9316993486aea7174ef9c5d36999bcdf3513
SalemFeyzo <f1989salim@gmail.com>
2024-08-14 18:36:26 +0300

    Hero animations

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import LottieAnimation from "@/components/lottie-animation";
 import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 
@@ -7,11 +8,12 @@ export default function Home({
   params: { locale: string };
 }) {
   unstable_setRequestLocale(locale);
-
-  const t = useTranslations("default.pages.home");
   return (
-    <div>
-      <h1>{t("title")}</h1>
+    <div className="flex flex-col md:flex-row justify-between items-center bg-gray-800 text-white h-2/5">
+      <div className="h-full ml-2 mr-2">Section</div>
+      <div className="h-full ml-2 mr-2">
+        <LottieAnimation />
+      </div>
     </div>
   );
 }
---
+++ components/lottie-animation.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import dynamic from "next/dynamic";
+
+// https://lottie.host/443825fc-7bd7-43e2-8d59-57b70b77b6c9/5HPPkFKEC1.json
+
+const DotLottieReact = dynamic(
+  () =>
+    import("@lottiefiles/dotlottie-react").then((mod) => mod.DotLottieReact),
+  {
+    ssr: false,
+  }
+);
+
+const LottieAnimation = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/443825fc-7bd7-43e2-8d59-57b70b77b6c9/5HPPkFKEC1.json"
+      loop={false}
+      autoplay
+    />
+  );
+};
+export default LottieAnimation;
