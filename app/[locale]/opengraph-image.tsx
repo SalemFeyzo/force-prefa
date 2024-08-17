31e6b0775b6875773a16e1494bb72b0533719d14
SalemFeyzo <f1989salim@gmail.com>
2024-08-17 15:34:35 +0300

    OpenGraph Image

--- app/[locale]/opengraph-image.tsx
+++ app/[locale]/opengraph-image.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+
 import { ImageResponse } from "next/og";
 import { getTranslations } from "next-intl/server";
+import { join } from "node:path";
+import { readFile } from "node:fs/promises";
 
 type Props = {
   params: {
@@ -14,20 +18,32 @@ export const size = {
 
 export default async function Image({ params: { locale } }: Props) {
   const t = await getTranslations({ locale, namespace: "default.openGraph" });
+  const logoData = await readFile(join(process.cwd(), "/public/logo.png"));
+  const logoSrc = Uint8Array.from(logoData).buffer;
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 128,
-          background: "white",
+          marginTop: 0,
+          background: "black",
+          color: "white",
           width: "100%",
           height: "100%",
           display: "flex",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
         }}
       >
-        {t("title")}
+        <h1
+          style={{
+            fontSize: "20px",
+          }}
+        >
+          {t("name")}
+        </h1>
+        {/* @ts-ignore */}
+        <img src={logoSrc} height={300} width={300} alt={t("name")} />
       </div>
     ),
     { ...size },
