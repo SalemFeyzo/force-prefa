0e30011175902742a84ab369cb2a06a2b3950ea6
SalemFeyzo <f1989salim@gmail.com>
2024-08-19 16:01:22 +0300

    Another try

--- app/[locale]/opengraph-image.tsx
+++ app/[locale]/opengraph-image.tsx
@@ -1,21 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 
 import { ImageResponse } from "next/og";
-import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+// import { getTranslations } from "next-intl/server";
 import { join } from "node:path";
 import { readFile } from "node:fs/promises";
-
-import { locales } from "@/lib/navigation";
 
 type Props = {
   params: {
     locale: string;
   };
 };
-
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
 
 export const size = {
   width: 1200,
@@ -25,13 +19,12 @@ export const size = {
 export const contentType = "image/png";
 
 export default async function Image({ params: { locale } }: Props) {
-  unstable_setRequestLocale(locale);
-  const t = await getTranslations({ locale, namespace: "default.openGraph" });
+  // const t = await getTranslations({ locale, namespace: "default.openGraph" });
   const logoData = await readFile(join(process.cwd(), "/assets/logo.png"));
   const logoSrc = Uint8Array.from(logoData).buffer;
 
-  const text =
-    locale === "ar" ? t("name").split(" ").reverse().join(" ") : t("name");
+  // const text =
+  // locale === "ar" ? t("name").split(" ").reverse().join(" ") : t("name");
 
   return new ImageResponse(
     (
@@ -50,8 +43,8 @@ export default async function Image({ params: { locale } }: Props) {
         }}
       >
         {/* @ts-ignore */}
-        <img src={logoSrc} height={300} width={300} alt={t("name")} />
-        <h1 tw="font-mono font-bold	">{text}</h1>
+        <img src={logoSrc} height={300} width={300} alt={"FORCE PREFABRIK"} />
+        {/* <h1 tw="font-mono font-bold	">{text}</h1> */}
       </div>
     ),
     {
