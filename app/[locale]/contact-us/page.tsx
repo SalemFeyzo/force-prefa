08bee7cd5bce82b8ee6e7478d28360a5c9ffe61e
SalemFeyzo <f1989salim@gmail.com>
2024-08-22 15:40:04 +0300

    Contact Us

--- app/[locale]/contact-us/page.tsx
+++ app/[locale]/contact-us/page.tsx
@@ -1,3 +1,4 @@
+import ContactForm from "@/components/contact-form";
 import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function ContactUs({
@@ -7,8 +8,44 @@ export default function ContactUs({
 }) {
   unstable_setRequestLocale(locale);
   return (
-    <div>
-      <h1>ContactUs</h1>
+    <div className="flex flex-col gap-3 bg-gray-800 text-gray-50">
+      <div className="flex h-full w-full flex-col gap-3 bg-gray-800 text-gray-50 md:flex-row">
+        <div className="w-full p-5">
+          <h1 className="border-b-2 pb-2 text-xl font-bold">
+            Contact Information
+          </h1>
+          <br />
+          <h2 className="text-lg">Address:</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non atque
+            nostrum expedita? Numquam ipsum rem exercitationem dolor eveniet
+            laudantium, esse ipsa nulla quae quidem? Dicta suscipit eos sed at
+            sequi?
+          </p>
+          <br />
+          <div className="flex items-baseline justify-start gap-2">
+            <h2 className="text-lg">Email:</h2>
+            <a href="email:info@form-prefabrik.com">info@force-prefabrik.com</a>
+          </div>
+          <br />
+          <div className="flex items-baseline justify-start gap-2">
+            <h2 className="text-lg">Phone Number:</h2>
+            <a href="tel:+905526100173" dir="ltr">
+              +90 552 610 01 73
+            </a>
+          </div>
+        </div>
+        <ContactForm />
+      </div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1948.1981648288775!2d36.13483627777435!3d36.237723319118984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1724329884052!5m2!1sen!2s"
+        className="w-full"
+        height="300"
+        // style="border:0;"
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
---
+++ components/contact-form.tsx
@@ -0,0 +1,148 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import validator from "validator";
+import { Button, Field, Label, Description, Input } from "@headlessui/react";
+
+const formSchema = z.object({
+  name: z.string().min(3),
+  company: z.string().min(3),
+  phoneNumber: z.string().min(10).max(15).refine(validator.isMobilePhone),
+  email: z.string().email(),
+  message: z.string().min(55),
+});
+
+export default function ContactForm() {
+  // 1. Define your form.
+  const {
+    register,
+    handleSubmit,
+    control,
+    setError,
+
+    formState: { isSubmitting, errors, isValidating, isValid },
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      company: "",
+      phoneNumber: "",
+      email: "",
+      message: "",
+    },
+  });
+  // 2. Define a submit handler.
+  const onSubmit: SubmitHandler<z.infer<typeof formSchema>> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log(data);
+    } catch (error) {
+      setError("root", {
+        message: "This email is already taken",
+      });
+    }
+  };
+  return (
+    <div className="w-full p-5">
+      <h1 className="border-b-2 pb-2 text-xl font-bold">Contact Form</h1>
+      <br />
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-full flex-col items-start justify-center gap-4 text-white"
+      >
+        <Field className="w-full">
+          <Label className="text-lg">Name:</Label>
+          <Description className="py-2 text-xs text-gray-400">
+            Use your real name.
+          </Description>
+          <Input
+            className="w-full rounded-md border p-3 data-[focus]:bg-gray-700 data-[hover]:shadow"
+            placeholder="Your Name..."
+            {...register("name")}
+            data-focus
+            data-hover
+          />
+        </Field>
+        <span className="text-red-500">
+          {errors.name && errors.name.message}
+        </span>
+        <Field className="w-full">
+          <Label className="text-lg">Company Name:</Label>
+          <Description className="py-2 text-xs text-gray-400">
+            Use your real Company Name.
+          </Description>
+          <Input
+            className="w-full rounded-md border p-3 data-[focus]:bg-gray-700 data-[hover]:shadow"
+            placeholder="Your Company Name"
+            {...register("company")}
+            data-focus
+            data-hover
+          />
+        </Field>
+        <span className="text-red-500">
+          {errors.company && errors.company.message}
+        </span>
+        <Field className="w-full">
+          <Label className="text-lg">Your Email:</Label>
+          <Description className="py-2 text-xs text-gray-400">
+            Use your Email.
+          </Description>
+          <Input
+            className="w-full rounded-md border p-3 data-[focus]:bg-gray-700 data-[hover]:shadow"
+            placeholder="example@gmail.com"
+            {...register("email")}
+            data-focus
+            data-hover
+          />
+        </Field>
+        <span className="text-red-500">
+          {errors.email && errors.email.message}
+        </span>
+        <Field className="w-full">
+          <Label className="text-lg">Your Phone Number:</Label>
+          <Description className="py-2 text-xs text-gray-400">
+            Use your phone number.
+          </Description>
+          <Input
+            className="w-full rounded-md border p-3 data-[focus]:bg-gray-700 data-[hover]:shadow"
+            placeholder="00905355556677"
+            {...register("phoneNumber")}
+            data-focus
+            data-hover
+          />
+        </Field>
+        <span className="text-red-500">
+          {errors.phoneNumber && errors.phoneNumber.message}
+        </span>
+        <Field className="w-full">
+          <Label className="text-lg">Your message:</Label>
+          <Description className="py-2 text-xs text-gray-400">
+            Tell us what you want.
+          </Description>
+          <textarea
+            className="h-40 w-full rounded-md border p-3 data-[focus]:bg-gray-700 data-[hover]:shadow"
+            placeholder="Your Message..."
+            {...register("message")}
+            data-focus
+            data-hover
+          />
+        </Field>
+        <span className="text-red-500">
+          {errors.message && errors.message.message}
+        </span>
+        <Button
+          disabled={isSubmitting}
+          type="submit"
+          className="w-full rounded bg-sky-600 px-4 py-2 text-sm text-white data-[active]:bg-sky-700 data-[hover]:bg-sky-500"
+        >
+          {isSubmitting ? "Loading..." : "Submit"}
+        </Button>
+        <span className="text-red-500">
+          {errors.root && errors.root.message}
+        </span>
+      </form>
+    </div>
+  );
+}
