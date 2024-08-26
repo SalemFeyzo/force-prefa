8b0d79dd50f2ad27b2ee7c1b999eeef212c2b562
SalemFeyzo <f1989salim@gmail.com>
2024-08-26 14:26:44 +0300

    fix

--- data/products.ts
+++ data/products.ts
@@ -1,40 +1,40 @@
-import { Product } from "@/types/product";
+// import { Product } from "@/types/product";
 
-export const productsEN: Product[] = [
-  {
-    id: "1",
-    title: "First Product",
-    description: "First product description",
-    mainImage: "/image.png",
-    images: ["/image.png", "/image.png", "/image.png"],
-    categoryId: "1",
-    featured: true,
-    locale: "en",
-  },
-];
+// export const productsEN: Product[] = [
+//   {
+//     id: "1",
+//     title: "First Product",
+//     description: "First product description",
+//     mainImage: "/image.png",
+//     images: ["/image.png", "/image.png", "/image.png"],
+//     categoryId: "1",
+//     featured: true,
+//     locale: "en",
+//   },
+// ];
 
-export const productsTR: Product[] = [
-  {
-    id: "1",
-    title: "First Product",
-    description: "First product description",
-    mainImage: "/image.png",
-    images: ["/image.png", "/image.png", "/image.png"],
-    categoryId: "1",
-    featured: true,
-    locale: "tr",
-  },
-];
+// export const productsTR: Product[] = [
+//   {
+//     id: "1",
+//     title: "First Product",
+//     description: "First product description",
+//     mainImage: "/image.png",
+//     images: ["/image.png", "/image.png", "/image.png"],
+//     categoryId: "1",
+//     featured: true,
+//     locale: "tr",
+//   },
+// ];
 
-export const productsAR: Product[] = [
-  {
-    id: "1",
-    title: "First Product",
-    description: "First product description",
-    mainImage: "/image.png",
-    images: ["/image.png", "/image.png", "/image.png"],
-    categoryId: "1",
-    featured: true,
-    locale: "ar",
-  },
-];
+// export const productsAR: Product[] = [
+//   {
+//     id: "1",
+//     title: "First Product",
+//     description: "First product description",
+//     mainImage: "/image.png",
+//     images: ["/image.png", "/image.png", "/image.png"],
+//     categoryId: "1",
+//     featured: true,
+//     locale: "ar",
+//   },
+// ];
